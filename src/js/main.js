84e18d143f8fedc1be8d0253b8fcfc079891f483
lassev05 <vestergaardlasse2@gmail.com>
2022-09-25 21:58:20 +0200

    Set token and check for tokens.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const getStarted = document.getElementById("movies-getStarted");
+
+const LOCAL_PREFIX = "movie-finder-";
+const RANDOM = Math.floor(Math.random() * 1000000);
+
+getStarted.addEventListener("click", () => {
+  const find = localStorage.getItem("token");
+
+  function set() {
+    localStorage.setItem("token", `${LOCAL_PREFIX}${RANDOM}`);
+  }
+
+  if (find) {
+    alert("You already have joined!");
+  } else {
+    set();
+  }
+});
