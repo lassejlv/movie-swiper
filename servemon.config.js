0ad01f8d1ec8e1844fb82fd7c2eaa9d451db5fe8
lassev05 <vestergaardlasse2@gmail.com>
2022-09-25 21:34:11 +0200

    Project Setup!

---
+++ servemon.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projectName: "movie-finder",
+  port: 3000, // Port to use
+  directory: "./src", // Directory to use
+  pkgManager: "servemon", // Package manager you want to use
+  watch: true, // Watch for changes
+  open: false, // Open the browser
+  fileExplore: true, // Enable file explorer
+  logger: false, // Enable logger
+};
