b7aea3fa9ac7ecb448182727c94d2e70dd1bf677
lassev05 <vestergaardlasse2@gmail.com>
2022-09-25 22:00:24 +0200

    Check if the user already have an token then remove the welcome page

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,25 @@
 const getStarted = document.getElementById("movies-getStarted");
+const Header = document.getElementById("header");
 
 const LOCAL_PREFIX = "movie-finder-";
 const RANDOM = Math.floor(Math.random() * 1000000);
 
+const find = localStorage.getItem("token");
+
+function set() {
+  localStorage.setItem("token", `${LOCAL_PREFIX}${RANDOM}`);
+}
+
 getStarted.addEventListener("click", () => {
-  const find = localStorage.getItem("token");
-
-  function set() {
-    localStorage.setItem("token", `${LOCAL_PREFIX}${RANDOM}`);
-  }
-
   if (find) {
     alert("You already have joined!");
   } else {
     set();
   }
 });
+
+if (find) {
+  Header.classList.add("hidden");
+} else {
+  Header.classList.remove("hidden");
+}
