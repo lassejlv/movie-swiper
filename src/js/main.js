659cb35043e0ccf0ca2a70200125c372189da57b
lassev05 <vestergaardlasse2@gmail.com>
2022-09-26 08:53:56 +0200

    Setup vite

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,22 @@
 const GetStarted = document.getElementById("movies-getStarted");
+const Logout = document.getElementById("movies-logout");
 const Header = document.getElementById("header");
+const Main = document.getElementById("main");
 
 const LOCAL_PREFIX = "movie-finder-";
-const RANDOM = Math.floor(Math.random() * 1000000);
+const RANDOM = Math.floor(Math.random() * 1000000000);
 
 const find = localStorage.getItem("token");
 
 function set(name, value) {
-  localStorage.setItem(name, `${LOCAL_PREFIX}`);
+  localStorage.setItem(name, `${LOCAL_PREFIX}${value}`);
 }
 
+function del(name) {
+  localStorage.removeItem(name);
+}
+
+// Start the token process
 GetStarted.addEventListener("click", () => {
   if (find) {
     alert("You already have joined!");
@@ -19,7 +26,15 @@ GetStarted.addEventListener("click", () => {
   }
 });
 
+// Delete the users token
+Logout.addEventListener("click", () => {
+  del("token");
+  window.location.reload();
+});
+
 // The users has a token
 if (find) {
   Header.remove();
+} else {
+  Main.remove();
 }
