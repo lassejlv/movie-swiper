2ff896786e461fb271c38a6e4d5a9aad428049ce
lassev05 <vestergaardlasse2@gmail.com>
2022-09-26 09:51:04 +0200

    Update .gitignore

--- src/js/main.js
+++ src/js/main.js
@@ -38,21 +38,3 @@ if (find) {
 } else {
   Main.remove();
 }
-
-// Fetch Dummy Data
-const data = [
-  {
-    name: "Shrek",
-    description:
-      "Shrek is a 2001 American computer-animated comedy film loosely based on the 1990 picture book of the same name by William Steig.",
-    image:
-      "https://upload.wikimedia.org/wikipedia/en/7/7b/Shrek_%282001_animated_feature_film%29.jpg",
-    url: "https://www.netflix.com/watch/60020686",
-  },
-];
-
-console.log(
-  data.map((d) => {
-    return d.name;
-  })
-);
