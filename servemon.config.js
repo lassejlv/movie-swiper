61d9c4dc09dd790a2c32bc5d63fcb34859d995bd
lassev05 <vestergaardlasse2@gmail.com>
2022-09-26 09:58:28 +0200

    Update servemon.config.js

--- servemon.config.js
+++ servemon.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   projectName: "movie-finder",
-  port: 3000, // Port to use
-  directory: "./src", // Directory to use
-  pkgManager: "servemon", // Package manager you want to use
-  watch: true, // Watch for changes
-  open: false, // Open the browser
-  fileExplore: true, // Enable file explorer
-  logger: false, // Enable logger
+  port: 3000,
+  directory: "./src",
+  watch: true,
+  fileExplore: true,
 };
