18bd95c24e302c60bd44fdfd4e548e0d2b585a52
lassev05 <vestergaardlasse2@gmail.com>
2022-09-28 21:07:37 +0200

    Added more movies, thank u to Sarah!

--- src/js/likes.js
+++ src/js/likes.js
@@ -10,10 +10,37 @@ if (!localStorage.getItem(LOCAL_PREFIX + "token")) {
 // Get the users likes
 const likes = JSON.parse(localStorage.getItem(`${LOCAL_PREFIX}returns`));
 
+console.log(likes);
+
 likes.map((like) => {
   const ul = document.getElementById("ul");
   const li = document.createElement("li");
 
-  li.innerText = `${like.Title}`;
+  li.setAttribute("id", like.ID);
+
+  li.innerHTML = `${like.Title} - <button id="delete"><i class="fa-solid fa-trash-alt"></i></button>`;
   ul.appendChild(li);
 });
+
+const DeleteButton = document.getElementById("delete");
+
+for (let i = 0; i < DeleteButton.length; i++) {
+  DeleteButton[i].addEventListener("click", (e) => {
+    const id = e.target.parentElement.getAttribute("id");
+
+    const items = JSON.parse(localStorage.getItem(`${LOCAL_PREFIX}returns`));
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id == id) {
+        items.splice(i, 1);
+      }
+    }
+    localStorage.setItem(`${LOCAL_PREFIX}returns`, JSON.stringify(items));
+    e.target.parentElement.remove();
+
+    window.reload();
+  }),
+    (e) => {
+      alert(e);
+    };
+}
